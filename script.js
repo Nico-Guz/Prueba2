5bcf4f74690510e3b4dbb9d4a0e3eeb0fa640a6f
Nico-Guz <nicolas.rinit@gmail.com>
2024-10-08 11:12:34 -0500

    commit inicial

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('miBoton').addEventListener('click', function() {
+    const texto = document.getElementById('texto');
+    texto.textContent = '¡El texto ha sido cambiado!';
+});
